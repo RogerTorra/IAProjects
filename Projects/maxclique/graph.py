2dc22ec0beecb5fac8388c66eaacbf7bd4bc955b
ROGER <elrogertorra@gmail.com>
2014-01-07 11:55:15 +0100

    practica 2

---
+++ Projects/maxclique/graph.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import subprocess
+class Graph:
+
+	def __init__(self):
+			self.nodes = []
+			self.edges = []
+			
+	def read(self,fname):
+			f = open(fname,"r")
+			for line in f:
+				l = line.split()
+				if l[0] == 'p':
+					self.nodes = range(1, int(l[2])+1)
+				elif l[0] == 'e':
+					self.edges.append([int(l[1]),int(l[2])])
+			f.close()
+
+	def maxclique_to_maxsat(self):
+
+		f = open("instance.wcnf", "w")
+		
+		T = len(self.nodes) + 1
+		E = ( len(self.nodes) * (len(self.nodes)-1) ) / 2
+		print >> f,'p wcnf',len(self.nodes),len(self.nodes) + (E - len(self.edges)), T
+
+		for n in self.nodes:
+			print >> f,1, n ,0
+		complete = []
+		for i in range(len(self.nodes) - 1):
+			for j in range(i+1, len(self.nodes)):
+				complete.append([self.nodes[i] , self.nodes[j]])
+		for e1,e2 in complete:
+			if [e1,e2] not in self.edges:
+				if [e2,e1] not in self.edges:
+					print >> f,T, -e1, -e2 ,0
+		f.close()
+
+	def solution(self,output):
+		for line in output:
+			l = line.split()
+			if l[0] == 'v':
+				print ",".join([e for e in l[1:] if int(e) > 0])
+
+
+if __name__ == "__main__":
+		g = Graph()
+		g.read(sys.argv[1])
+		g.maxclique_to_maxsat()
+		command = '../maxsat/maxsatz2009 instance.wcnf'		
+		output = os.popen(command).readlines()
+		g.solution(output)
+
+
--- Projects/maxsat/maxsat.py
+++ Projects/maxsat/maxsat.py
@@ -1,31 +1,58 @@
-class maxsat:
+import sys
+class MaxSAT:
+    def __init__(self):
+        self.soft = []
+        self.hard = []
+        self.infinit = -1
+	    self.nvar = 0
 
-	def alo(self,lits):
-		self.hard.append(lits)
-	def amo(self,lits):
-		#self.hard.append([(-x,-y) for x in range(len(lits) -1) for y in range(i+1, len(lits))])
-		for i in range(len(lits)):
-			for j in range(i+1, len(lits)):
-					self.hard.append([-lits[i],-lits[j]])
-	def eo(self,lits):
-		self.alo(lits)
-		self.amo(lits)
+    def read(self, fname):
+        f = open(fname, "r")
+        for line in f:
+            l = line.split()
+            if l[0] != 'c':
+                if l[0] == 'p':
+                    infinit = l[4]
+		    self.nvar = int(l[2])
+                elif l[0] == infinit:
+                    self.hard.append([int(c) for c in l[1:-1]])
+                else:
+                    self.soft.append(([int(c) for c in l[1:-1]], int(l[0])))
+        f.close()
+        print "Soft: ", self.soft
+        print "Hard: ", self.hard
 
-	def toSat(self, fname):
-		f = open(fname, "w")
-		#header
-		print >> f,"p cnf", self.nvar, len(self.soft) + len(self.hard)
+    def alo(self, lits):
+        self.hard.append(lits)
+ 
+    def amo(self, lits):
+        for i in range(len(lits) - 1):
+            for j in range(i+1, len(lits)):
+                self.hard.append([-lits[i], -lits[j]])
+    def eo(self, lits):
+        self.alo(lits)
+        self.amo(lits)
+    
+    def tosat(self, fname):
+	f = open(fname,"w")
+	# header
+	print >>f, "p cnf", self.nvar, len(self.soft)+len(self.hard)
+	
+	# soft
+	for c, w in self.soft:
+		for lit in c:
+			print >>f, lit,
+		print >>f, "0"		
+	# hard
+	for c in self.hard:
+		for lit in c:
+			print >>f, lit,
+		print >>f, "0"
 
-		#soft
-		for c, w in self.soft:
-			for lit in c:
-				print >> f,lit,
-			print >> f,"0"
+     	f.close()
 
-		#hard
-		for c, w in self.hard:
-			for lit in c:
-				print >> f,lit,
-			print >> f,"0"
-		f.close()
 
+if __name__ == "__main__":
+    sat = MaxSAT()
+    sat.read(sys.argv[1])
+    sat.tosat("prueba.cnf")
